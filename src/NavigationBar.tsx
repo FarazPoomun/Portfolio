8d261c352ef47f0d16cf52a386970d2672a59b0c
Faraz Poomun <farazpoomun2k@gmail.com>
2024-05-12 15:34:59 +0400

    Fix layout

--- src/NavigationBar.tsx
+++ src/NavigationBar.tsx
@@ -1,62 +1,64 @@
-import { faLightbulb } from '@fortawesome/free-regular-svg-icons';
-import { faLightbulb as solidFaLightBulb } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLightbulb } from "@fortawesome/free-regular-svg-icons";
+import { faLightbulb as solidFaLightBulb } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-import './NavigationBar.css';
-import { useEffect, useState } from 'react';
+import "./NavigationBar.css";
+import { useEffect, useState } from "react";
 
-function NavigationBar(){
-    const darkModeCSSClass = 'dark-mode';
-    const lightModeCSSClass = 'light-mode';
+function NavigationBar() {
+  const darkModeCSSClass = "dark-mode";
+  const lightModeCSSClass = "light-mode";
 
-const [isDarkModeEnabled, setIsDarkModeEnabled] = useState<boolean>(true);
+  const [isDarkModeEnabled, setIsDarkModeEnabled] = useState<boolean>(true);
 
-useEffect(() =>{
-   const storageValue = localStorage.getItem("theme-css-value");
+  useEffect(() => {
+    const storageValue = localStorage.getItem("theme-css-value");
+    let CSSmodeClass = lightModeCSSClass;
 
-   if(storageValue){
-if(storageValue != darkModeCSSClass){
-       setIsDarkModeEnabled(false);
-       localStorage.setItem("theme-css-value", lightModeCSSClass);
-}else{
-    manageRootTheme(darkModeCSSClass);
-    localStorage.setItem("theme-css-value", darkModeCSSClass);
-}
-   }else{
-    manageRootTheme(darkModeCSSClass);
-    localStorage.setItem("theme-css-value", darkModeCSSClass);
-   }
-}, []);
+    if (storageValue) {
+      if (storageValue != darkModeCSSClass) {
+        setIsDarkModeEnabled(false);
+      } else {
+        CSSmodeClass = darkModeCSSClass;
+      }
+    } else {
+        CSSmodeClass = darkModeCSSClass;
+    }
+    manageRootTheme(CSSmodeClass);
+    localStorage.setItem("theme-css-value", CSSmodeClass);
+  }, []);
 
-useEffect(() =>{
-    let CSSmodeClass = isDarkModeEnabled? darkModeCSSClass: lightModeCSSClass;
-manageRootTheme(CSSmodeClass);
-localStorage.setItem("theme-css-value", CSSmodeClass);
-}, [isDarkModeEnabled])
+  useEffect(() => {
+    let CSSmodeClass = isDarkModeEnabled ? darkModeCSSClass : lightModeCSSClass;
+    manageRootTheme(CSSmodeClass);
+    localStorage.setItem("theme-css-value", CSSmodeClass);
+  }, [isDarkModeEnabled]);
 
-
-
-const manageRootTheme = (themeClass: string): void => {
+  const manageRootTheme = (themeClass: string): void => {
     const rootElem = document.querySelector(":root");
 
-    if(rootElem){
-        rootElem.classList.remove(darkModeCSSClass);
-        rootElem.classList.remove(lightModeCSSClass);
-        rootElem.classList.add(themeClass);
+    if (rootElem) {
+      rootElem.classList.remove(darkModeCSSClass);
+      rootElem.classList.remove(lightModeCSSClass);
+      rootElem.classList.add(themeClass);
     }
-}
+  };
 
-const themeSwitcherClicked = () =>{
+  const themeSwitcherClicked = () => {
     setIsDarkModeEnabled(!isDarkModeEnabled);
-}
+  };
 
-    return (
-      <>
-      <div className='navbar-container'>
-        <FontAwesomeIcon icon={isDarkModeEnabled? faLightbulb: solidFaLightBulb} className='theme-switcher' onClick={themeSwitcherClicked} />
-        </div>
-      </>
-    );
+  return (
+    <>
+      <div className="navbar-container">
+        <FontAwesomeIcon
+          icon={isDarkModeEnabled ? faLightbulb : solidFaLightBulb}
+          className="theme-switcher"
+          onClick={themeSwitcherClicked}
+        />
+      </div>
+    </>
+  );
 }
 
 export default NavigationBar;
