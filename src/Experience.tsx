cbec9885962514ad033a5956683ed865066a0683
Faraz Poomun <farazpoomun2k@gmail.com>
2024-05-14 00:03:26 +0400

    add timeline animation

--- src/Experience.tsx
+++ src/Experience.tsx
@@ -5,22 +5,32 @@ import ExperienceModel from "./interfaces/Experience";
 function Experience() {
   const experienceData: ExperienceModel[] = [
     {
-      From: 2012,
+      From: 2023,
       JobTitle: "Senior Full-Stack Developer",
       Company: "Objectivity (Part of Accenture)",
       CompanyUrl: "https://www.accenture.com/",
-      Languages: [".Net", "MongoDb", "Azure", "Docker", "React"],
-      Description: "Did SomeStuffs",
+      Languages: [".Net", "MongoDb", "Azure", "Docker", "React", "JS", "TS"],
+      Description: "Build and maintain a future state architecture web application that is meant to replace a legacy system for the client." +
+      "Experienced with both the Backend and the Frontend aspect of the application",
     },
     {
-      From: 2010,
-      To: 2012,
+      From: 2020,
+      To: 2023,
       JobTitle: "Senior Full-Stack Developer",
       Company: "Dayforce",
       CompanyUrl: "https://www.dayforce.com/",
-      Languages: [".Net", "SQL", "Dojo"],
+      Languages: [".Net", "SQL", "Dojo", "JS", "TS"],
       Description: "Did Some more Stuffs",
     },
+    {
+        From: 2018,
+        To: 2018,
+        JobTitle: "Internship",
+        Company: "Dayforce",
+        CompanyUrl: "https://www.dayforce.com/",
+        Languages: [".Net", "SQL", "HTML", "JS", "CSS", "MVC"],
+        Description: "Did Some more Stuffs",
+      },
   ];
   return (
     <>
--- src/ExperienceTimelineItem.tsx
+++ src/ExperienceTimelineItem.tsx
@@ -1,8 +1,8 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import ExperienceModel from "./interfaces/Experience";
 import { faMinus, faLocationArrow } from "@fortawesome/free-solid-svg-icons";
-import { useState } from "react";
-
+import {useRef, useState } from "react";
+import './ExperienceTimeline.css';
 interface ExperienceTimelineItemProp {
   experienceModel: ExperienceModel;
 }
@@ -11,20 +11,33 @@ function ExperienceTimelineItem({
   experienceModel,
 }: ExperienceTimelineItemProp) {
   const [displayRedirect, setDisplayRedirect] = useState<boolean>(false);
+  const timelineNode = useRef<HTMLDivElement>(null);
+  const wholeExperienceContainer = useRef<HTMLDivElement>(null);
 
   const redirect = (companyUrl: string) => {
     window.open(companyUrl, "_blank");
   };
 
+  const onExperienceHover= ()=> {
+    setDisplayRedirect(true);
+    timelineNode && timelineNode.current && timelineNode.current.classList.add("blue-bg");
+    wholeExperienceContainer && wholeExperienceContainer.current && wholeExperienceContainer.current.classList.add("experience-shadow");
+  }
+
+  const onExperienceOut= ()=> {
+    setDisplayRedirect(false);
+    timelineNode && timelineNode.current && timelineNode.current.classList.remove("blue-bg");
+    wholeExperienceContainer && wholeExperienceContainer.current && wholeExperienceContainer.current.classList.remove("experience-shadow");
+  }
+
   return (
-    <li className="mb-10 ms-4">
-      <div className="absolute w-3 h-3 bg-gray-500 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+    <li className="mb-10 ms-4" onMouseOver={onExperienceHover} onMouseOut={onExperienceOut}>
+      <div ref={timelineNode} className="absolute w-3 h-3 bg-gray-500 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+      <div ref={wholeExperienceContainer} className="padding-1">
       <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
         {experienceModel.From} - {experienceModel.To ?? "CURRENT"}
       </time>
       <h3
-        onMouseOver={() => setDisplayRedirect(true)}
-        onMouseOut={() => setDisplayRedirect(false)}
         className="text-lg font-semibold"
       >
         {experienceModel.JobTitle}{" "}
@@ -43,10 +56,11 @@ function ExperienceTimelineItem({
       </p>
       <div className="card-languages">
         {experienceModel.Languages.map((language) => (
-          <span className="bg-green-100 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-green-900 dark:text-green-300">
+          <span key={experienceModel.Company+language} className="bg-green-100 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-green-900 dark:text-green-300">
             {language}
           </span>
         ))}
+      </div>
       </div>
     </li>
   );
